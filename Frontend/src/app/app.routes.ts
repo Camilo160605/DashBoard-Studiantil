7f3fd4780333bea0996e69cddd18cb93ddf3ca05
Camilo160605 <camilo1665ober@gmail.com>
2025-11-23 18:07:50 -0500

    ¿?

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,20 +1,10 @@
 import { Routes } from '@angular/router';
-import { DashboardPageComponent } from './pages/dashboard/dashboard-page.component';
-import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  {
-    path: 'auth',
-      loadChildren: () => 
-        import('./auth/auth.routes').then(m => m.authRoutes)
-  },
-  {
-    path: 'dashboard',
-    component: DashboardPageComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path:'**',
-    redirectTo: 'auth'
-  },
+
+    {
+        path: '',
+        loadChildren: () => import('./auth/auth.routes')
+    }
+
 ];
